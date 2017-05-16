f39c18a930e6e38d72d7139286448b75eec3ccdf
KooroshRZ <koorosh.rajabzadeh@gmail.com>
2017-05-16 08:43:52 +0430

    multi_thread added

--- client.py
+++ client.py
@@ -1,18 +1,22 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '172.24.34.110'
-port = 3000
+data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '172.24.36.112'
+data_port = 3020
+control_port = 3021
 
-s.connect((host, port))
-recieved = s.recv(1024)
+control_socket.connect((host, control_port))
+data_socket.connect((host, data_port))
+
+recieved = data_socket.recv(1024)
 print recieved
 
-if recieved != "":
-    while True:
-        command = raw_input('>> ')
-        s.send(command.encode())
-        if (command == 'LIST'):
-            result = s.recv(1024)
-            print result
-s.close()
+# if recieved != "":
+    # while True:
+    #     # command = raw_input('>> ')
+    #     # # s.send(command.encode())
+    #     # if (command == 'LIST'):
+    #     #     result = s.recv(1024)
+    # s.close()
+    #         print result
--- server.py
+++ server.py
@@ -1,27 +1,62 @@
 import socket
-import os
-import glob
+# import os
+# import glob
+from threading import Thread
+from SocketServer import ThreadingMixIn
 
-def handle(command):
-    if command == 'LIST':
-        files = os.listdir(path)
-        for file in files:
-            c.send(file.encode())
+class ClientThread(Thread):
+    def __init__(self, conn, IP, PORT):
+        Thread.__init__(self)
+        self.IP = IP
+        self.PORT = PORT
+        self.conn = conn
+        print "[+] New server socket thread started for " + IP + ":" + str(PORT)
+    def run(self):
+        while True:
+            command = conn.recv(2048)
+            print "Server received data:", data
+            MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit:")
+            if MESSAGE == 'exit':
+                break
+            conn.send(MESSAGE)
+            #implement commands
 
+# def handle(command):
+#     if command == 'LIST':
+#         files = os.listdir(path)
+#         for file in files:
+#             c.send(file.encode())
 
-ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-host = '172.24.34.110'
-port = 3000
-ss.bind((host, port))
-
-ss.listen(5)
-c, addr = ss.accept()
-print 'connection from ', addr
-c.send('thnx for connecting')
-
-
+data_port = 3020
+control_port = 3021
+host = '172.24.36.112'
 path = 'files/'
+
+
+data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+data_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+data_socket.bind((host, data_port))
+
+control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+control_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+control_socket.bind((host, control_port))
+
+data_threads = []
+control_threads = []
+data_socket.listen(10)
+control_socket.listen(10)
+
 while True:
-    command = c.recv(1024)
-    handle(command)
+    print "Multithreded Python server : waiting for connections from clients"
+    (conn, (IP, PORT)) = data_socket.accept()
+    new_thread = ClientThread(conn, IP, PORT)
+    new_thread.start()
+    data_threads.append(new_thread)
+
+    (conn, (IP, PORT)) = control_socket.accept()
+    new_thread = ClientThread(conn, IP, PORT)
+    new_thread.start()
+    control_threads.append(new_thread)
+
+for t in threads:
+    t.join()
