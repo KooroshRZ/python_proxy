d15389fe3e385cfdbe851eb8c51bfb7903a69a0f
KooroshRZ <koorosh.rajabzadeh@gmail.com>
2017-05-17 23:00:32 +0430

    RETR command completed

--- client.py
+++ client.py
@@ -11,11 +11,35 @@ control_port = 3021
 control_socket.connect((host, control_port))
 data_socket.connect((host, data_port))
 
-while True:
+#######################################################################################################
+# web_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# web_socket.connect(("192.168.128.30", 80))
+# web_socket.sendall("GET /~94131090/CN1_Project_Files/flower.jpeg HTTP/1.1\r\nHost: 192.168.128.30\r\n\r\n".encode())
+# result = web_socket.recv(65536)
+# print(result)
+# web_socket.close()
+#
+# # print("result: " + str(result))
+# file = open('flower.jpeg', 'wb+')
+# file.write(result)
+# file.close()
+######################################################################################################
 
+def recvall(size, data_socket):
+    data = bytes()
+    while len(data) < size:
+        packet = data_socket.recv(size - len(data))
+        if not packet:
+            return None
+        data += packet
+
+    return data
+
+
+while True:
     command = input('>> ')
     raw_command = command.split()[0]
-    control_socket.sendall(raw_command.encode())
+    control_socket.send(command.encode())
 
     if raw_command == 'QUIT':
     	break
@@ -25,9 +49,18 @@ while True:
         print(result)
 
     elif raw_command == 'RETR':
+
         file_name = command.split()[1]
-        control_socket.sendall(file_name.encode())
-        result = data_socket.recv(4096).decode()
-        file = open(path + file_name, 'wb+')
-        file.write(result.encode())
-        file.close()
+        code = control_socket.recv(1024).decode()
+
+        
+        if code == '150':
+        	size = control_socket.recv(1024).decode()
+	        data = recvall(int(size), data_socket)
+	        file = open(path + file_name, 'wb+')
+	        file.write(data)
+	        file.close()
+	        print("file downloaded successfully!")
+        elif code == '550':
+        	print("file not found")
+
--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import socket
-import select
 import os
-# import glob
+import time
 from threading import Thread
 from socketserver import ThreadingMixIn
 
@@ -14,41 +13,114 @@ path = 'files/'
 
 class ClientThread(Thread):
 
-    def __init__(self, control_conn, data_conn, IP):
+    def __init__(self, control_conn, data_conn, web_socket, IP):
         Thread.__init__(self)
         self.IP = IP
         self.data_conn = data_conn
         self.control_conn = control_conn
+        self.web_socket = web_socket
+
         print("[+] New client connected " + IP)
 
+    def recvall(self, size):
+        data = bytes()
+        while len(data) < size:
+            packet = self.web_socket.recv(size - len(data))
+            if not packet:
+                return None
+            data += packet
+            print(len(data))
+
+        return data
+
+    def get_from_server(self, file_name):
+
+        URL_head = "HEAD /~94131090/CN1_Project_Files/" + str(file_name) + " HTTP/1.1\r\nHost: 192.168.128.30\r\n\r\n"
+        URL_get = "GET /~94131090/CN1_Project_Files/" + str(file_name) + " HTTP/1.1\r\nHost: 192.168.128.30\r\n\r\n"
+
+        self.web_socket.connect(("192.168.128.30", 80))
+        self.web_socket.send(URL_head.encode())
+        result = self.web_socket.recv(4096).decode()
+        size_i = result.find('Content-Length')+16
+        size_j = result.find('Content-Type')-2
+        size = int(result[size_i:size_j])
+
+        head_size = size_j + 30
+        size = size + head_size
+
+        self.web_socket.send(URL_get.encode())
+        data = self.recvall(size)
+        web_socket.close()
+
+        file = open(path + str(file_name), 'wb+')
+        file.write(data[head_size:size])
+        file.close()
+
+    def send_file(self, file_name):
+        files = os.listdir(path)
+
+        #send local
+        for file in files:
+            if file_name == file:
+                file = open(path + file_name, 'rb')
+                if file != "":
+                    data = file.read(65536)
+                    code = 150
+                    self.control_conn.send(str(code).encode())
+                    time.sleep(1)
+                    self.control_conn.send(str(len(data)).encode())
+                    self.data_conn.send(data)
+                else:
+                    code = 550
+                    self.control_conn.sendall(code.encode())
+                return
+
+        # send remote
+        self.get_from_server(file_name)
+        file = open(path + file_name, 'rb')
+        if file != "":
+            data = file.read(65536)
+            code = 150
+            self.control_conn.send(str(code).encode())
+            time.sleep(1)
+            self.control_conn.send(str(len(data)).encode())
+            self.data_conn.send(data)
+        else:
+            code = 550
+            self.control_conn.send(str(code).encode())
+
+
     def do_command(self, command):
-        
-        if command == 'LIST':
+        print(command)
+        raw_command = command.split()[0]
+        if raw_command == 'LIST':
             files = os.listdir(path)
-            for file in files:
-                self.control_conn.sendall(file.encode())
-                self.control_conn.sendall("\n".encode())
-                
-        if command == 'RETR':
-            file_name = self.control_conn.recv(4096).decode()
-            print("file name" + file_name)
-            file = open(path + file_name, 'r')
-            result = file.read(1000)
-            self.data_conn.sendall(result.encode())
+            #list files from server
+
+        if raw_command == 'RETR':
+            print("retr")
+            file_name = command.split()[1]
+            self.send_file(file_name)
+            print("file " + file_name + " downloaded by client " + IP)
+            
 
     def run(self):
         while True:
-            command = self.control_conn.recv(4096).decode()
+            command = self.control_conn.recv(1024).decode()
             self.do_command(command)
 	    
-
+#data socket
 data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 data_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 data_socket.bind((host, data_port))
 
+#control socket
 control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 control_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 control_socket.bind((host, control_port))
+
+#web server socket
+web_socket = socket.socket(socket.AF_INET, socket.SOL_SOCKET)
 
 threads = []
 data_socket.listen(10)
@@ -62,6 +134,8 @@ C_PORTs = []
 
 i=0
 
+print("waiting for connections !")
+
 while True:
 
     (data_conn, (IP, D_PORT)) = data_socket.accept()
@@ -69,7 +143,7 @@ while True:
     control_conns.append(control_conn)
     data_conns.append(data_conn)
 
-    new_thread = ClientThread(control_conns[i], data_conns[i], IP)
+    new_thread = ClientThread(control_conns[i], data_conns[i], web_socket, IP)
     new_thread.start()
     threads.append(new_thread)
     i += 1
