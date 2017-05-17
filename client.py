61c407159a3063b5edcb82bca14f0f2816c4617e
KooroshRZ <koorosh.rajabzadeh@gmail.com>
2017-05-17 07:44:42 +0430

    RETR command

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 
+path = 'client_files/'
+
 data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '172.24.36.112'
@@ -9,14 +11,21 @@ control_port = 3021
 control_socket.connect((host, control_port))
 data_socket.connect((host, data_port))
 
-recieved = data_socket.recv(1024)
-print recieved
+while True:
+    command = raw_input('>> ')
+    raw_command = command.split()[0]
+	
+    control_socket.send(raw_command.encode())
 
-# if recieved != "":
-    # while True:
-    #     # command = raw_input('>> ')
-    #     # # s.send(command.encode())
-    #     # if (command == 'LIST'):
-    #     #     result = s.recv(1024)
-    # s.close()
-    #         print result
+    if raw_command == 'LIST':
+		result = control_socket.recv(2048)
+		print result
+
+    if raw_command == 'RETR':
+		file_name = command.split()[1]
+		control_socket.send(file_name.encode())
+		result = data_socket.recv(65536)
+		print result
+		file = open(path + file_name, 'wb+')
+		file.write(result)
+		file.close
--- server.py
+++ server.py
@@ -1,36 +1,44 @@
 import socket
-# import os
+import os
 # import glob
 from threading import Thread
 from SocketServer import ThreadingMixIn
 
-class ClientThread(Thread):
-    def __init__(self, conn, IP, PORT):
-        Thread.__init__(self)
-        self.IP = IP
-        self.PORT = PORT
-        self.conn = conn
-        print "[+] New server socket thread started for " + IP + ":" + str(PORT)
-    def run(self):
-        while True:
-            command = conn.recv(2048)
-            print "Server received data:", data
-            MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit:")
-            if MESSAGE == 'exit':
-                break
-            conn.send(MESSAGE)
-            #implement commands
-
-# def handle(command):
-#     if command == 'LIST':
-#         files = os.listdir(path)
-#         for file in files:
-#             c.send(file.encode())
 
 data_port = 3020
 control_port = 3021
 host = '172.24.36.112'
 path = 'files/'
+
+class ClientThread(Thread):
+
+    def __init__(self, control_conn, data_conn, IP, PORT):
+        Thread.__init__(self)
+        self.IP = IP
+        self.PORT = PORT
+        self.data_conn = data_conn
+        self.control_conn = control_conn
+        print "[+] New server socket thread started for " + IP + ":" + str(PORT)
+
+    def do_command(self, command):
+        if command == "LIST":
+            files = os.listdir(path)
+            for file in files:
+                control_conn.send(file.encode())
+                control_conn.send("\n".encode())
+        if command == "RETR":
+            file_name = control_conn.recv(1024)
+            print "file name : " ,file_name
+            file = open(path + file_name, 'r')
+            result = file.read(1000)
+            data_conn.send(result.encode())
+
+
+    def run(self):
+        while True:
+            command = self.control_conn.recv(1024)
+            self.do_command(command)
+	    
 
 
 data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,22 +49,23 @@ control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 control_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 control_socket.bind((host, control_port))
 
-data_threads = []
-control_threads = []
+threads = []
+# control_threads = []
 data_socket.listen(10)
 control_socket.listen(10)
 
 while True:
     print "Multithreded Python server : waiting for connections from clients"
-    (conn, (IP, PORT)) = data_socket.accept()
-    new_thread = ClientThread(conn, IP, PORT)
-    new_thread.start()
-    data_threads.append(new_thread)
+    (data_conn, (IP, PORT)) = data_socket.accept()
+    (control_conn, (IP, PORT)) = control_socket.accept()
 
-    (conn, (IP, PORT)) = control_socket.accept()
-    new_thread = ClientThread(conn, IP, PORT)
+    new_thread = ClientThread(control_conn, data_conn, IP, PORT)
     new_thread.start()
-    control_threads.append(new_thread)
+    threads.append(new_thread)
+
+    # new_thread = ClientThread(conn, IP, PORT)
+    # new_thread.start()
+    # control_threads.append(new_thread)
 
 for t in threads:
     t.join()
